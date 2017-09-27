644bb97da3e1d0f733ba51c26161651edf9417ab
Chance Phiniezy <phiniezyc@gmail.com>
2017-09-27 19:06:56 -0400

    added initial firebase connection, but not fully working yet.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,24 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyDgY_I2hZW76jmFJ4Fvqn7hGoXF_oFF-h8",
+    authDomain: "trainscheduleactivity.firebaseapp.com",
+    databaseURL: "https://trainscheduleactivity.firebaseio.com",
+    projectId: "trainscheduleactivity",
+    storageBucket: "trainscheduleactivity.appspot.com",
+    messagingSenderId: "736662195923"
+  };
+  firebase.initializeApp(config);
+
+  var dataRef = firebase.database();
+
+  var trainName = "";
+  var trainDestination = "";
+  var firstTrainTime = "";
+  var trainFrequency = 0;
+
+  $("#addTrainSubmitButton").on("click", function(event) {
+    event.preventDefault();
+    
+
+  })
 
