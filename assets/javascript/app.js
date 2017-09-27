c7e73a2ab1f98b77e23c578035f67ea4d64353aa
Chance Phiniezy <phiniezyc@gmail.com>
2017-09-27 19:23:31 -0400

    connected firebase w/ on click event

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,36 @@
- // Initialize Firebase
- var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyDgY_I2hZW76jmFJ4Fvqn7hGoXF_oFF-h8",
     authDomain: "trainscheduleactivity.firebaseapp.com",
     databaseURL: "https://trainscheduleactivity.firebaseio.com",
     projectId: "trainscheduleactivity",
     storageBucket: "trainscheduleactivity.appspot.com",
     messagingSenderId: "736662195923"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var dataRef = firebase.database();
+var dataRef = firebase.database();
 
-  var trainName = "";
-  var trainDestination = "";
-  var firstTrainTime = "";
-  var trainFrequency = 0;
+var trainName = "";
+var trainDestination = "";
+var firstTrainTime = "";
+var trainFrequency = 0 + " minutes";
 
-  $("#addTrainSubmitButton").on("click", function(event) {
+$("#addTrainSubmitButton").on("click", function (event) {
     event.preventDefault();
-    
 
-  })
+    trainName = $("#trainNameFormInput").val().trim();
+    trainDestination = $("#trainDestinationFormInput").val().trim();
+    firstTrainTime = $("#firstTrainTimeInput").val().trim();
+    trainFrequency = $("#trainFrequency").val().trim();
+
+    // Code for the push
+    dataRef.ref().push({
+
+        trainName: trainName,
+        trainDestination: trainDestination,
+        firstTrainTime: firstTrainTime,
+        trainFrequency: trainFrequency,
+    });
+})
 
