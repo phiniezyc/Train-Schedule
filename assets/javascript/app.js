47aff21c486ec0a1c7f56535e6ef588aef7cca7e
Chance Phiniezy <phiniezyc@gmail.com>
2017-09-28 01:09:35 -0400

    appends to table correctly, but for some reason frequency does not show up in table and/or console

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,7 +14,7 @@ var dataRef = firebase.database();
 var trainName = "";
 var trainDestination = "";
 var firstTrainTime = "";
-var trainFrequency = 0 + " minutes";
+var trainFrequency = 0;
 
 $("#addTrainSubmitButton").on("click", function (event) {
     event.preventDefault();
@@ -26,7 +26,6 @@ $("#addTrainSubmitButton").on("click", function (event) {
 
     // Code for the push
     dataRef.ref().push({
-
         trainName: trainName,
         trainDestination: trainDestination,
         firstTrainTime: firstTrainTime,
@@ -34,3 +33,61 @@ $("#addTrainSubmitButton").on("click", function (event) {
     });
 })
 
+// Firebase watcher + initial loader.
+dataRef.ref().on("child_added", function(childSnapshot){
+    //log everything that's coming out of snapshot
+    console.log(childSnapshot.val().trainName);
+    console.log(childSnapshot.val().trainDestination);
+    console.log(childSnapshot.val().firstTrainTime);
+    console.log(childSnapshot.val().trainFrequency);
+
+    var currentTrainName = childSnapshot.val().trainName;
+    var currentTrainDestination = childSnapshot.val().trainDestination; 
+    var currentFirstTrainTime = childSnapshot.val().firstTrainTime;
+    var currentTrainFrequency = childSnapshot.val().trainFrequency;
+
+    //Should this be ordered by next arrival?
+    $("table").find("tbody").append([
+        "<tr>",
+            "<td>" + currentTrainName,
+            "<td>" + currentTrainDestination,
+            "<td>" + currentTrainFrequency, 
+            "<td>",
+            "<td>",
+    ].join(""));
+})
+
+
+
+
+    // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value"). 
+    
+    /*dataRef.ref().on("child_added", function(childSnapshot) {
+        
+              // Log everything that's coming out of snapshot
+              console.log(childSnapshot.val().name);
+              console.log(childSnapshot.val().name);
+              console.log(childSnapshot.val().email);
+              console.log(childSnapshot.val().age);
+              console.log(childSnapshot.val().comment);
+              console.log(childSnapshot.val().joinDate);
+        
+              // full list of items to the well
+              $("#full-member-list").append("<div class='well'><span id='name'> " + childSnapshot.val().name +
+                " </span><span id='email'> " + childSnapshot.val().email +
+                " </span><span id='age'> " + childSnapshot.val().age +
+                " </span><span id='comment'> " + childSnapshot.val().comment + " </span></div>");
+        
+            // Handle the errors
+            }, function(errorObject) {
+              console.log("Errors handled: " + errorObject.code);
+            });
+        
+            dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+        
+              // Change the HTML to reflect
+              $("#name-display").html(snapshot.val().name);
+              $("#email-display").html(snapshot.val().email);
+              $("#age-display").html(snapshot.val().age);
+              $("#comment-display").html(snapshot.val().comment);
+            }); */
